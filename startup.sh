c64a3384bbfa6653512d26ebf437c256425e25df
Bjorn Robertsson <bjorn@coder.com>
2025-04-16 11:50:00 +0100

    Update startup.sh
    Revert on failure added

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CONFIG_FILE="/etc/containerd/config.toml"
+PAGER=cat
 
 # Ensure the config file exists
 sudo touch "$CONFIG_FILE"
@@ -31,15 +32,45 @@ EOF
   insecure_skip_verify = true
 EOF
   fi
+ sleep 10
+ systemctl restart containerd
+ sleep 2
+ systemctl status containerd
+ return $?
 }
 
+backup_toml() {
+ # Backup original config
+ if [ ! -f "$BACKUP_TOML" ]; then
+    cp "$CONTAINERD_TOML" "$BACKUP_TOML"
+ fi
+}
+
+revert_toml() {
+    echo "Reverting containerd config..."
+    cp "$BACKUP_TOML" "$CONTAINERD_TOML"
+    systemctl restart containerd
+    if systemctl is-active --quiet containerd; then
+        echo "containerd successfully reverted and running."
+	exit 0
+    else
+        echo "containerd failed to start even after reverting. Manual intervention needed."
+    fi
+}
 # ---- Registry Mirror List ----
-add_mirror_block "docker-registry.docker-registry.svc.local:5000" "http://docker-registry.docker-registry.svc.local:5000"
-add_mirror_block "docker-direct.local:80" "http://docker-direct.local:80"
-add_mirror_block "docker.local" "https://docker.local" true
-add_mirror_block "artifactory.local" "http://artifactory.local"
-add_mirror_block "docker-direct.local" "http://docker-direct.local"
-add_mirror_block "192.168.2.250:5000" "http://192.168.2.250:5000"
+
+CONTAINERD_TOML="/etc/containerd/config.toml"
+BACKUP_TOML="/etc/containerd/config.toml.bak"
+
+backup_toml
+
+# add_mirror_block "docker-registry.docker-registry.svc.local:5000" "http://docker-registry.docker-registry.svc.local:5000" || revert_toml
+add_mirror_block "docker-registry.docker-registry.svc.local:5000" "http://docker-registry.docker-registry.svc.local:5000" || revert_toml
+add_mirror_block "docker-direct.local:80" "http://docker-direct.local:80" || revert_toml
+add_mirror_block "docker.local" "https://docker.local" true || revert_toml
+add_mirror_block "artifactory.local" "http://artifactory.local" || revert_toml
+add_mirror_block "docker-direct.local" "http://docker-direct.local" || revert_toml
+add_mirror_block "192.168.2.250:5000" "http://192.168.2.250:5000" || revert_toml
 
 echo "Done patching $CONFIG_FILE"
 
